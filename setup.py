c0ffc550825cbb9f96286241cc85245753df3728
Thanachart Ritbumroong <thanachart.rit@gmail.com>
2021-08-25 17:05:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setuptools.setup(
     author_email='thanachart.rit@gmail.com',  # TODO: update email
     description='Personal package for Thanachart',
     long_description_content_type='text/markdown',
-    url='https://github.com/happilyeverafter95/pdf-wrangler',
-    install_requires=['chardet==3.0.4', 'pdfminer.six==20181108'],
     project_urls={
         'Main': 'https://github.com/thanachart/thanachart',
     },
@@ -21,7 +19,4 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
-    python_requires='>=3.6',
 )
