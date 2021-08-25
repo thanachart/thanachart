7d118ce278521e0c0359247f89a8b16a7fc3fc89
Thanachart Ritbumroong <thanachart.rit@gmail.com>
2021-08-25 16:56:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ import setuptools
 
 VERSION = '0.0.1'
 
-
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
-
-
 setuptools.setup(
     name='thanachart',
     version=VERSION,
     author='Thanachart Ritbumroong',
     author_email='thanachart.rit@gmail.com',  # TODO: update email
     description='Personal package for Thanachart',
-    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/happilyeverafter95/pdf-wrangler',
     install_requires=['chardet==3.0.4', 'pdfminer.six==20181108'],
